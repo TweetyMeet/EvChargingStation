a59d16df7544c4365397533ee8e7761c57571794
TweetyMeet <meetpansuriya6008@gmail.com>
2023-02-23 11:16:11 +0530

    test5

--- lib/screens/tag_screen/tag_screen.dart
+++ lib/screens/tag_screen/tag_screen.dart
@@ -86,13 +86,90 @@ class _TagScreenState extends State<TagScreen> with SingleTickerProviderStateMix
                        child: Card(
                          elevation: 7,
                          shape: RoundedRectangleBorder(
-                           side: BorderSide(
-                             color: Colors.greenAccent,
-                           ),
                            borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
                          ),
                          margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                         child: Text(''),
+                         child:  Stack(
+                           children: [
+                             Column(
+                               crossAxisAlignment: CrossAxisAlignment.start,
+                               children: [
+                                 Padding(
+                                   padding: const EdgeInsets.all(15.0),
+                                   child: Container(
+                                     width: 250,
+                                     height: 120,
+                                     decoration: BoxDecoration(
+                                         color: Color(0xffCCE1F4),
+                                         borderRadius: BorderRadius.circular(10)),
+                                     child: Padding(
+                                       padding: const EdgeInsets.only(top: 10, left: 10),
+                                       child: Column(
+                                         crossAxisAlignment: CrossAxisAlignment.start,
+                                         children: [
+                                           Text(
+                                             'OLA',
+                                             style: TextStyle(
+                                                 fontSize: 22,
+                                                 fontWeight: FontWeight.w600),
+                                           ),
+                                           Text(
+                                             'Electronic',
+                                             style: TextStyle(
+                                                 fontSize: 22,
+                                                 fontWeight: FontWeight.w600),
+                                           ),
+                                           Text(
+                                             'Scooter',
+                                             style: TextStyle(
+                                                 fontSize: 22,
+                                                 fontWeight: FontWeight.w600),
+                                           ),
+                                         ],
+                                       ),
+                                     ),
+                                   ),
+                                 ),
+                                 Padding(
+                                   padding: const EdgeInsets.only(left: 20),
+                                   child: Row(
+                                     children: [
+                                       Icon(
+                                         Icons.location_on,
+                                         color: Color(0xffCCE1F4),
+                                       ),
+                                       Text(
+                                         '4750 Arbutus #312, Vancouver',
+                                         style: TextStyle(fontSize: 15),
+                                       ),
+                                     ],
+                                   ),
+                                 ),
+                                 SizedBox(
+                                   height: 5,
+                                 ),
+                                 Padding(
+                                   padding: const EdgeInsets.only(left: 20),
+                                   child: Row(
+                                     children: [
+                                       Icon(
+                                         Icons.bolt,
+                                         color: Color(0xffCCE1F4),
+                                       ),
+                                       Text(
+                                         'Type 2',
+                                         style: TextStyle(fontSize: 18, color: Colors.grey),
+                                       ),
+                                     ],
+                                   ),
+                                 ),
+                               ],
+                             ),
+                             Positioned(
+                                 left: 130,
+                                 child: Image(image: AssetImage('assets/images/bike_image-removebg-preview.png'))),
+                           ],
+                         ),
 
                        ),
                      );
@@ -109,13 +186,91 @@ class _TagScreenState extends State<TagScreen> with SingleTickerProviderStateMix
                            child: Card(
                              elevation: 7,
                              shape: RoundedRectangleBorder(
-                               side: BorderSide(
-                                 color: Colors.greenAccent,
-                               ),
+
                                borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
                              ),
                              margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                             child: Text(''),
+                             child:  Stack(
+                               children: [
+                                 Column(
+                                   crossAxisAlignment: CrossAxisAlignment.start,
+                                   children: [
+                                     Padding(
+                                       padding: const EdgeInsets.all(15.0),
+                                       child: Container(
+                                         width: 250,
+                                         height: 120,
+                                         decoration: BoxDecoration(
+                                             color: Color(0xffCCE1F4),
+                                             borderRadius: BorderRadius.circular(10)),
+                                         child: Padding(
+                                           padding: const EdgeInsets.only(top: 10, left: 10),
+                                           child: Column(
+                                             crossAxisAlignment: CrossAxisAlignment.start,
+                                             children: [
+                                               Text(
+                                                 'OLA',
+                                                 style: TextStyle(
+                                                     fontSize: 22,
+                                                     fontWeight: FontWeight.w600),
+                                               ),
+                                               Text(
+                                                 'Electronic',
+                                                 style: TextStyle(
+                                                     fontSize: 22,
+                                                     fontWeight: FontWeight.w600),
+                                               ),
+                                               Text(
+                                                 'Scooter',
+                                                 style: TextStyle(
+                                                     fontSize: 22,
+                                                     fontWeight: FontWeight.w600),
+                                               ),
+                                             ],
+                                           ),
+                                         ),
+                                       ),
+                                     ),
+                                     Padding(
+                                       padding: const EdgeInsets.only(left: 20),
+                                       child: Row(
+                                         children: [
+                                           Icon(
+                                             Icons.location_on,
+                                             color: Color(0xffCCE1F4),
+                                           ),
+                                           Text(
+                                             '4750 Arbutus #312, Vancouver',
+                                             style: TextStyle(fontSize: 15),
+                                           ),
+                                         ],
+                                       ),
+                                     ),
+                                     SizedBox(
+                                       height: 5,
+                                     ),
+                                     Padding(
+                                       padding: const EdgeInsets.only(left: 20),
+                                       child: Row(
+                                         children: [
+                                           Icon(
+                                             Icons.bolt,
+                                             color: Color(0xffCCE1F4),
+                                           ),
+                                           Text(
+                                             'Type 2',
+                                             style: TextStyle(fontSize: 18, color: Colors.grey),
+                                           ),
+                                         ],
+                                       ),
+                                     ),
+                                   ],
+                                 ),
+                                 Positioned(
+                                     left: 130,
+                                     child: Image(image: AssetImage('assets/images/bike_image-removebg-preview.png'))),
+                               ],
+                             ),
 
                            ),
                          );
@@ -131,13 +286,91 @@ class _TagScreenState extends State<TagScreen> with SingleTickerProviderStateMix
                            child: Card(
                              elevation: 7,
                              shape: RoundedRectangleBorder(
-                               side: BorderSide(
-                                 color: Colors.greenAccent,
-                               ),
+
                                borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
                              ),
                              margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                             child: Text(''),
+                             child:  Stack(
+                               children: [
+                                 Column(
+                                   crossAxisAlignment: CrossAxisAlignment.start,
+                                   children: [
+                                     Padding(
+                                       padding: const EdgeInsets.all(15.0),
+                                       child: Container(
+                                         width: 250,
+                                         height: 120,
+                                         decoration: BoxDecoration(
+                                             color: Color(0xffCCE1F4),
+                                             borderRadius: BorderRadius.circular(10)),
+                                         child: Padding(
+                                           padding: const EdgeInsets.only(top: 10, left: 10),
+                                           child: Column(
+                                             crossAxisAlignment: CrossAxisAlignment.start,
+                                             children: [
+                                               Text(
+                                                 'OLA',
+                                                 style: TextStyle(
+                                                     fontSize: 22,
+                                                     fontWeight: FontWeight.w600),
+                                               ),
+                                               Text(
+                                                 'Electronic',
+                                                 style: TextStyle(
+                                                     fontSize: 22,
+                                                     fontWeight: FontWeight.w600),
+                                               ),
+                                               Text(
+                                                 'Scooter',
+                                                 style: TextStyle(
+                                                     fontSize: 22,
+                                                     fontWeight: FontWeight.w600),
+                                               ),
+                                             ],
+                                           ),
+                                         ),
+                                       ),
+                                     ),
+                                     Padding(
+                                       padding: const EdgeInsets.only(left: 20),
+                                       child: Row(
+                                         children: [
+                                           Icon(
+                                             Icons.location_on,
+                                             color: Color(0xffCCE1F4),
+                                           ),
+                                           Text(
+                                             '4750 Arbutus #312, Vancouver',
+                                             style: TextStyle(fontSize: 15),
+                                           ),
+                                         ],
+                                       ),
+                                     ),
+                                     SizedBox(
+                                       height: 5,
+                                     ),
+                                     Padding(
+                                       padding: const EdgeInsets.only(left: 20),
+                                       child: Row(
+                                         children: [
+                                           Icon(
+                                             Icons.bolt,
+                                             color: Color(0xffCCE1F4),
+                                           ),
+                                           Text(
+                                             'Type 2',
+                                             style: TextStyle(fontSize: 18, color: Colors.grey),
+                                           ),
+                                         ],
+                                       ),
+                                     ),
+                                   ],
+                                 ),
+                                 Positioned(
+                                     left: 130,
+                                     child: Image(image: AssetImage('assets/images/bike_image-removebg-preview.png'))),
+                               ],
+                             ),
 
                            ),
                          );
