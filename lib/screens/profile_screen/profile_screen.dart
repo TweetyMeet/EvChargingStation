87e447824a92a5e6b042ce49fc249541328c4172
TweetyMeet <meetpansuriya6008@gmail.com>
2023-02-23 10:37:40 +0530

    test3

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ev_project/screens/Login_Screen/Log_in.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,123 +12,160 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
+    return  SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white70,
+        body: Column(
           children: [
+            Row(children :[
+              SizedBox(width: 16,),
+              Icon(Icons.arrow_back_ios_outlined),
+              SizedBox(width: 16,),
+              Text('Profile',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            ],
+            ),
+            CircleAvatar(
+              radius: 40,
+              backgroundImage:  AssetImage("assets/PHOTO.png")),
+
+            SizedBox(height: 10,),
+            Text('Meet Pansuriya',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+            Text('India',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+            SizedBox(height: 20,),
+            ListTile(
+
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.tealAccent.withOpacity(0.1),
+                ),
+                child: Icon(Icons.man_rounded,color: Colors.black,),
+              ),
+              title: Text('My Profile',),
+              trailing: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+                child: Icon(Icons.arrow_forward_ios_rounded),
+              ),
+
+            ),
+            ListTile(
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.tealAccent.withOpacity(0.1),
+                ),
+                child: Icon(Icons.save_sharp,color: Colors.black,),
+              ),
+              title: Text('Saved Slots',),
+              trailing: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+                child: Icon(Icons.arrow_forward_ios_rounded),
+              ),
+
+            ),
+            ListTile(
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.tealAccent.withOpacity(0.1),
+                ),
+                child: Icon(Icons.bookmark,color: Colors.black,),
+              ),
+              title: Text('My Booking',),
+              trailing: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+                child: Icon(Icons.arrow_forward_ios_rounded),
+              ),
+
+            ),
+            ListTile(
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.tealAccent.withOpacity(0.1),
+                ),
+                child: Icon(Icons.car_crash_sharp,color: Colors.black,),
+              ),
+              title: Text('My Car',),
+              trailing: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+                child: Icon(Icons.arrow_forward_ios_rounded),
+              ),
+
+            ),
+            ListTile(
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.tealAccent.withOpacity(0.1),
+                ),
+                child: Icon(Icons.settings,color: Colors.black,),
+              ),
+              title: Text('Setting',),
+              trailing: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+                child: Icon(Icons.arrow_forward_ios_rounded),
+              ),
+
+            ),
+            SizedBox(height: 54,),
+
             Container(
-              width: double.infinity,
-              height: 310,
-              child: Stack(
-                children: [
-                  Positioned(
-                    bottom: 70,
-                    right: 0,
-                    left: 0,
-                    child: Container(
-                      width: 450,
-                      height: 450,
-                      decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(250),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                              spreadRadius: 3,
-                              blurRadius: 5,
-                            ),
-                          ]),
-                    ),
-                  ),
-                  Positioned(
-                    top: 150,
-                    left: 110,
-                    child: Container(
-                      width: 150,
-                      height: 150,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(75),
-                          color: Colors.black,
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/circleavatar.png'),
-                              fit: BoxFit.cover),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                              spreadRadius: 3,
-                              blurRadius: 5,
-                              offset: Offset(2, 1),
-                            ),
-                          ]),
-                    ),
-                  ),
-                ],
+              height: 50,
+              width: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.black,
+
               ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'FirstName',
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'LastName',
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Birth Date',
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Name',
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-            ),
+              child: OutlinedButton(
+
+                onPressed: (){
+
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> LogIN()));
+
+                  },
+                child: Text('Log Out',style: TextStyle(color: Colors.white),),
+              )
+            )
           ],
         ),
+
       ),
     );
   }
--- lib/screens/tag_screen/tag_screen.dart
+++ lib/screens/tag_screen/tag_screen.dart
@@ -8,11 +8,143 @@ class TagScreen extends StatefulWidget {
   State<TagScreen> createState() => _TagScreenState();
 }
 
-class _TagScreenState extends State<TagScreen> {
+class _TagScreenState extends State<TagScreen> with SingleTickerProviderStateMixin {
+  late  TabController _tabController = TabController(length: 3, vsync: this);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('tag'),),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey,
+       body: Padding(
+         padding: const EdgeInsets.all(8.0),
+         child :Column(
+             children: [
+             Row(
+               children :[
+               SizedBox(width: 16,),
+               Icon(Icons.arrow_back_ios_outlined),
+               SizedBox(width: 16,),
+               Text('My  Booking',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+             ],
+             ),
+               Padding(
+                 padding: const EdgeInsets.all(20.0),
+                 child: TextFormField(
+
+                   decoration: InputDecoration(
+                     prefixIcon: Icon(Icons.search),
+                     hintText: "Search",
+
+                     border: OutlineInputBorder(
+                       borderRadius: BorderRadius.circular(20)
+                     )
+                   ),
+                 ),
+               ),
+               SizedBox(height: 4,),
+               Card(
+                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                 elevation: 5,
+                 child: Container(
+
+                 decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(12),
+                   color: Colors.black.withOpacity(0.1),
+                 ),
+
+                 child: TabBar(
+                   controller: _tabController,
+                   isScrollable: true,
+                   labelPadding: EdgeInsets.symmetric(horizontal: 35),
+                   indicator: BoxDecoration(
+                     borderRadius: BorderRadius.circular(12),
+                     color: Colors.black
+                   ),
+
+                   tabs: [
+                     Tab(text: "Ongoing",),
+                     Tab(text: "Booked",),
+                     Tab(text: "History",),
+
+                   ],
+                 ) ,),
+               ),
+               Expanded(
+                 child: TabBarView(
+                   controller: _tabController,
+                   children: [
+                 ListView.builder(
+                   physics: BouncingScrollPhysics(),
+                   itemCount: 10,
+                   itemBuilder: (context,index){
+                     return Container(
+                       height: 250,
+                       width: 250,
+                       child: Card(
+                         shape: RoundedRectangleBorder(
+                           side: BorderSide(
+                             color: Colors.greenAccent,
+                           ),
+                           borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
+                         ),
+                         margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                         child: Text(''),
+
+                       ),
+                     );
+                   },
+                 ),
+                     ListView.builder(
+                       physics: BouncingScrollPhysics(),
+                       itemCount: 10,
+                       itemBuilder: (context,index){
+                         return Container(
+                           height: 250,
+                           width: 250,
+
+                           child: Card(
+                             shape: RoundedRectangleBorder(
+                               side: BorderSide(
+                                 color: Colors.greenAccent,
+                               ),
+                               borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
+                             ),
+                             margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                             child: Text(''),
+
+                           ),
+                         );
+                       },
+                     ),
+                     ListView.builder(
+                       physics: BouncingScrollPhysics(),
+                       itemCount: 10,
+                       itemBuilder: (context,index){
+                         return Container(
+                           height: 250,
+                           width: 250,
+                           child: Card(
+                             shape: RoundedRectangleBorder(
+                               side: BorderSide(
+                                 color: Colors.greenAccent,
+                               ),
+                               borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
+                             ),
+                             margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                             child: Text(''),
+
+                           ),
+                         );
+                       },
+                     ),
+                 ],),
+               ),
+           ],),
+         ),
+       ),
+
     );
   }
 }
+
